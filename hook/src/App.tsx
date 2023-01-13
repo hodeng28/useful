989dd55d695118de32ffa2e803f4b134c94d0293
hodeng28 <ghepd28@gmail.com>
2023-01-14 01:25:51 +0900

    mui init

--- hook/src/App.tsx
+++ hook/src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Button, Stack } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>hook</div>
-    </div>
+    <Stack>
+      <Button>test</Button>
+    </Stack>
   );
 }
 
